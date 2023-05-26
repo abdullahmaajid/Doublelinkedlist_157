ff04e6f5131901ff209e3e5e3a136dd7a012053c
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-26 11:29:39 +0700

    // Inserting a Node Beetwen The Nodes in the List //   Node * current = START; //step1.a  Node* previous = NULL; //step1.b  while (current->next != NULL && current->next->noMhs < nim) //step1.c  {   previous = current; //1.d   current = current->next; //1.e  }
     if (current->next != NULL && nim == current->next->noMhs) {
      cout << "\nDuplicate roll numbers not allowed" << endl;
      return;
     }

--- Doublelinkedlist_157/Doublelinkedlist_157.cpp
+++ Doublelinkedlist_157/Doublelinkedlist_157.cpp
@@ -52,5 +52,21 @@ void DoubleLinkedList::addNote() {
 		START = newNode;												// Step 6
 		return;
 	}
-	/* Insert a node in the begining of a doubly - Linked List */
+
+	// Inserting a Node Beetwen The Nodes in the List //
+		Node * current = START; //step1.a
+	Node* previous = NULL; //step1.b
+	while (current->next != NULL && current->next->noMhs < nim) //step1.c
+	{
+		previous = current; //1.d
+		current = current->next; //1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next; //step4
+	newNode->prev = current; //step5
 }

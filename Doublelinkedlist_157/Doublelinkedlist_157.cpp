d87561acb64a49f8dae28d3f90199718af9c9967
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-26 11:19:04 +0700

    /* Insert a node in the begining of a doubly - Linked List */  if (START == NULL || nim <= START->noMhs) {   if (START != NULL && nim == START->noMhs) {    cout << "\n Duplicate number not allowed" << endl;    return;   }   newNode->next = START;           // Step 3   if (START != NULL)    START->prev = newNode;          // Step 4   newNode->prev = NULL;           // Step 5   START = newNode;            // Step 6   return;  }

--- Doublelinkedlist_157/Doublelinkedlist_157.cpp
+++ Doublelinkedlist_157/Doublelinkedlist_157.cpp
@@ -25,7 +25,7 @@ public:
 };
 
 DoubleLinkedList::DoubleLinkedList() {
-	START = NULL
+	START = NULL;
 }
 
 void DoubleLinkedList::addNote() {
@@ -37,4 +37,20 @@ void DoubleLinkedList::addNote() {
 	cin >> nm;
 	Node* newNode = new Node();											// Step 1
 	newNode->noMhs = nim;												// Step 2
+	newNode->name = nm;													// Step 2
 
+	/* Insert a node in the begining of a doubly - Linked List */
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\n Duplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;											// Step 3
+		if (START != NULL)
+			START->prev = newNode;										// Step 4
+		newNode->prev = NULL;											// Step 5
+		START = newNode;												// Step 6
+		return;
+	}
+	/* Insert a node in the begining of a doubly - Linked List */
+}
